b2ef5f58855870eea7d335e2deeeee35215a9c65
whid-bb <whid11s50@gmail.com>
2022-07-28 12:41:25 +0200

    fix test error

--- src/tests/List.test.js
+++ src/tests/List.test.js
@@ -1,6 +1,7 @@
 import renderer from 'react-test-renderer';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../redux/configureStore';
 import List from '../Components/List/List';
 
@@ -8,7 +9,10 @@ describe('List test', () => {
   it('List render correctly', () => {
     const ListPage = renderer.create(
       <Provider store={store}>
-        <List />
+        <Router>
+
+          <List />
+        </Router>
       </Provider>,
     ).toJSON();
     expect(ListPage).toMatchSnapshot();
@@ -17,7 +21,10 @@ describe('List test', () => {
   test('Render List component', () => {
     render(
       <Provider store={store}>
-        <List />
+        <Router>
+
+          <List />
+        </Router>
       </Provider>,
     );
   });
