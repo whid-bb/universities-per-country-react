aef1241f2658d0d39013423017a3ef7f5eaacbbc
whid-bb <whid11s50@gmail.com>
2022-07-28 08:01:50 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.scss';
+import Home from './Components/Home/Home';
+import Details from './Components/Details/Details';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route exact path="/" element={<Home />}>
+          <Route path="/country-details/:country" element={<Details />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { StyledEngineProvider } from '@mui/material/styles';
+import store from './redux/configureStore';
 import './index.scss';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Provider store={store}>
+        <StyledEngineProvider injectFirst>
+          <App />
+        </StyledEngineProvider>
+      </Provider>
+    </React.StrictMode>
+  </BrowserRouter>,
 );
