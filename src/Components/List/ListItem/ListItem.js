f0acab96b8629f090b4b3b034b93e71c0183530a
whid-bb <whid11s50@gmail.com>
2022-07-28 11:34:48 +0200

    add back button

--- src/Components/List/ListItem/ListItem.js
+++ src/Components/List/ListItem/ListItem.js
@@ -1,11 +1,12 @@
 /* eslint-disable max-len, no-unused-vars */
-import { Link } from 'react-router-dom';
+import { Link, useMatch } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import ArrowCircleRightOutlinedIcon from '@mui/icons-material/ArrowCircleRightOutlined';
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import { searchCountries } from '../../../redux/countries/Countries';
-import { fetchCountriesDetails } from '../../../redux/countries/CountriesService';
+import fetchCountries, { fetchCountriesDetails } from '../../../redux/countries/CountriesService';
 import './ListItem.scss';
 
 function ListItem({ country }) {
@@ -15,8 +16,13 @@ function ListItem({ country }) {
     dispatch(fetchCountriesDetails());
   }, []);
 
+  const match = useMatch('/country-details/:country');
   const handleClick = (e) => {
     dispatch(searchCountries(e));
+  };
+
+  const getHome = () => {
+    dispatch(fetchCountries());
   };
 
   const groupItems = () => country.reduce((rows, key, index) => (index % 2 === 0 ? rows.push([{ ...key, ...{ id: index } }])
@@ -27,13 +33,22 @@ function ListItem({ country }) {
       <div className="country-list-container">
         {groupItems().map((el) => (
           <div className="row" key={`${el[0].id}`}>
+            {match && (
+              <Link
+                to="/"
+                onClick={() => getHome()}
+                className="go-back-ico"
+              >
+                <ArrowBackIosIcon />
+              </Link>
+            )}
             {el.map((el) => (
               <Link key={el.id} onClick={() => handleClick(el)} className="country-list-link" to={`/country-details/${el.name}`}>
                 <div className="country-list-item">
                   <div className="cli-top">
-                    {/* <img src={flag} alt={el.name} /> */}
+
                     <img src={`https://countryflagsapi.com/png/${el.abbr}`} alt={el.name} />
-                    <ArrowCircleRightOutlinedIcon />
+                    {!match && <ArrowCircleRightOutlinedIcon />}
                   </div>
                   <div className="cli-data">
                     <p>
